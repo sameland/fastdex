c972740a97fb6983492de2801b7c6a262652079b
typ0520 <yapeng.tong@baidao.comc>
2017-05-02 18:51:47 +0800

    ins

--- gradle-plugin/src/main/groovy/com/dx168/fastdex/build/task/FastdexInstantRunTask.groovy
+++ gradle-plugin/src/main/groovy/com/dx168/fastdex/build/task/FastdexInstantRunTask.groovy
@@ -7,7 +7,6 @@ import com.dx168.fastdex.build.util.FastdexUtils
 import com.dx168.fastdex.build.util.GradleUtils
 import com.dx168.fastdex.build.util.MetaInfo
 import com.dx168.fastdex.build.variant.FastdexVariant
-import fastdex.build.lib.aapt.FileUtil
 import fastdex.build.lib.fd.Communicator
 import fastdex.build.lib.fd.ServiceCommunicator
 import fastdex.common.ShareConstants
@@ -23,6 +22,9 @@ public class FastdexInstantRunTask extends DefaultTask {
     FastdexVariant fastdexVariant
     File resourceApFile
     String resDir
+    boolean alreadySendPatch
+    IDevice device
+    String packageName
 
     FastdexInstantRunTask() {
         group = 'fastdex'
@@ -42,37 +44,31 @@ public class FastdexInstantRunTask extends DefaultTask {
         }
     }
 
-    IDevice preparedDevice() {
+    def preparedDevice() {
+        if (device != null) {
+            return
+        }
         AndroidDebugBridge.initIfNeeded(false)
         AndroidDebugBridge bridge =
                 AndroidDebugBridge.createBridge("/Users/tong/Applications/android-sdk-macosx/platform-tools/adb", false)
         waitForDevice(bridge)
         IDevice[] devices = bridge.getDevices()
-        IDevice device = null
-
         if (devices != null && devices.length > 0) {
+            if (devices.length > 1) {
+                throw new FastdexRuntimeException("Find multiple devices!!")
+            }
             device = devices[0]
         }
 
         if (device == null) {
             throw new FastdexRuntimeException("Device not found!!")
         }
-
-        if (devices.length > 1) {
-            throw new FastdexRuntimeException("Find multiple devices!!")
-        }
         project.logger.error("==fastdex device connected ${device.toString()}")
-        return device
     }
 
     public void onDexGenerateSuccess(boolean nornalBuild,boolean dexMerge) {
-
-    }
-
-    @TaskAction
-    void instantRun() {
-        IDevice device = preparedDevice()
-        String packageName = GradleUtils.getPackageName(project.android.sourceSets.main.manifest.srcFile.absolutePath)
+        preparedDevice()
+        packageName = GradleUtils.getPackageName(project.android.sourceSets.main.manifest.srcFile.absolutePath)
         ServiceCommunicator serviceCommunicator = new ServiceCommunicator(packageName)
         try {
             boolean active = false
@@ -110,7 +106,7 @@ public class FastdexInstantRunTask extends DefaultTask {
                 changeCount += 1
             }
 
-            boolean result = serviceCommunicator.talkToService(device, new Communicator<Boolean>() {
+            serviceCommunicator.talkToService(device, new Communicator<Boolean>() {
                 @Override
                 public Boolean communicate(DataInputStream input, DataOutputStream output) throws IOException {
                     output.writeInt(ProtocolConstants.MESSAGE_PATCHES)
@@ -143,41 +139,133 @@ public class FastdexInstantRunTask extends DefaultTask {
                     return input.readBoolean()
                 }
             })
-            if (result) {
-                project.logger.error("==fastdex send patch data success.....")
+            project.logger.error("==fastdex send patch data success.....")
 
-                //kill app
-                killApp(appPid)
-                startBootActivity(packageName)
-            }
-            else {
-                project.logger.error("==fastdex send patch data fail.....")
-                normalRun(device,packageName,fastdexVariant.metaInfo.variantName)
-            }
+            //kill app
+            killApp(appPid)
+            startBootActivity(packageName)
+
+            project.tasks.getByName("validateSigning${fastdexVariant.variantName}").enabled = false
+            project.tasks.getByName("package${fastdexVariant.variantName}").enabled = false
+            project.tasks.getByName("assemble${fastdexVariant.variantName}").enabled = false
+            alreadySendPatch = true
         } catch (IOException e) {
             if (fastdexVariant.configuration.debug) {
                 e.printStackTrace()
             }
-            //TODO 选择一个variant
-            normalRun(device,packageName,"Debug")
         }
     }
 
-    void normalRun(IDevice device,String packageName,String targetVariantName) {
-        def targetVariant = null
-        project.android.applicationVariants.all { variant ->
-            def variantName = variant.name.capitalize()
-            if (variantName.equals(targetVariantName)) {
-                targetVariant = variant
-            }
+    @TaskAction
+    void instantRun() {
+        if (alreadySendPatch) {
+            return
         }
 
-        project.logger.error("==fastdex normal run ${targetVariantName}")
+        preparedDevice()
+        if (packageName == null) {
+            packageName = GradleUtils.getPackageName(project.android.sourceSets.main.manifest.srcFile.absolutePath)
+        }
+        normalRun(device,packageName)
+//        IDevice device = preparedDevice()
+//        String packageName = GradleUtils.getPackageName(project.android.sourceSets.main.manifest.srcFile.absolutePath)
+//        ServiceCommunicator serviceCommunicator = new ServiceCommunicator(packageName)
+//        try {
+//            boolean active = false
+//            int appPid = -1
+//            MetaInfo runtimeMetaInfo = serviceCommunicator.talkToService(device, new Communicator<MetaInfo>() {
+//                @Override
+//                public MetaInfo communicate(DataInputStream input, DataOutputStream output) throws IOException {
+//                    output.writeInt(ProtocolConstants.MESSAGE_PING)
+//                    MetaInfo runtimeMetaInfo = new MetaInfo()
+//                    active = input.readBoolean()
+//                    runtimeMetaInfo.buildMillis = input.readLong()
+//                    runtimeMetaInfo.variantName = input.readUTF()
+//                    appPid = input.readInt()
+//                    return runtimeMetaInfo
+//                }
+//            })
+//            project.logger.error("==fastdex receive: ${runtimeMetaInfo}")
+//            if (fastdexVariant.metaInfo.buildMillis != runtimeMetaInfo.buildMillis) {
+//                throw new IOException("buildMillis not equal")
+//            }
+//            if (!fastdexVariant.metaInfo.variantName.equals(runtimeMetaInfo.variantName)) {
+//                throw new IOException("variantName not equal")
+//            }
+//
+//            File resourcesApk = FastdexUtils.getResourcesApk(project,fastdexVariant.variantName)
+//            generateResourceApk(resourcesApk)
+//            File mergedPatchDex = FastdexUtils.getMergedPatchDex(fastdexVariant.project,fastdexVariant.variantName)
+//            File patchDex = FastdexUtils.getPatchDexFile(fastdexVariant.project,fastdexVariant.variantName)
+//
+//            int changeCount = 1
+//            if (FileUtils.isLegalFile(mergedPatchDex)) {
+//                changeCount += 1
+//            }
+//            if (FileUtils.isLegalFile(patchDex)) {
+//                changeCount += 1
+//            }
+//
+//            boolean result = serviceCommunicator.talkToService(device, new Communicator<Boolean>() {
+//                @Override
+//                public Boolean communicate(DataInputStream input, DataOutputStream output) throws IOException {
+//                    output.writeInt(ProtocolConstants.MESSAGE_PATCHES)
+//                    output.writeLong(0L)
+//                    output.writeInt(changeCount)
+//
+//                    project.logger.error("==fastdex write ${ShareConstants.RESOURCE_APK_FILE_NAME}")
+//                    output.writeUTF(ShareConstants.RESOURCE_APK_FILE_NAME)
+//                    byte[] bytes = FileUtils.readContents(resourcesApk)
+//                    output.writeInt(bytes.length)
+//                    output.write(bytes)
+//                    if (FileUtils.isLegalFile(mergedPatchDex)) {
+//                        project.logger.error("==fastdex write ${mergedPatchDex}")
+//                        output.writeUTF(ShareConstants.MERGED_PATCH_DEX)
+//                        bytes = FileUtils.readContents(mergedPatchDex)
+//                        output.writeInt(bytes.length)
+//                        output.write(bytes)
+//                    }
+//                    if (FileUtils.isLegalFile(patchDex)) {
+//                        project.logger.error("==fastdex write ${patchDex}")
+//                        output.writeUTF(ShareConstants.PATCH_DEX)
+//                        bytes = FileUtils.readContents(patchDex)
+//                        output.writeInt(bytes.length)
+//                        output.write(bytes)
+//                    }
+//
+//                    output.writeInt(ProtocolConstants.UPDATE_MODE_WARM_SWAP)
+//                    output.writeBoolean(true)
+//
+//                    return input.readBoolean()
+//                }
+//            })
+//            if (result) {
+//                project.logger.error("==fastdex send patch data success.....")
+//
+//                //kill app
+//                killApp(appPid)
+//                startBootActivity(packageName)
+//            }
+//            else {
+//                project.logger.error("==fastdex send patch data fail.....")
+//                normalRun(device,packageName,fastdexVariant.metaInfo.variantName)
+//            }
+//        } catch (IOException e) {
+//            if (fastdexVariant.configuration.debug) {
+//                e.printStackTrace()
+//            }
+//            //TODO 选择一个variant
+//            normalRun(device,packageName,"Debug")
+//        }
+    }
+
+    void normalRun(IDevice device,String packageName) {
+        def targetVariant = fastdexVariant.androidVariant
+        project.logger.error("==fastdex normal run ${fastdexVariant.variantName}")
         //安装app
         File apkFile = targetVariant.outputs.first().getOutputFile()
         project.logger.error("adb install -r ${apkFile}")
         device.installPackage(apkFile.absolutePath,true)
-
         startBootActivity(packageName)
     }
 
